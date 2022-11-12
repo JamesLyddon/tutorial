9fc47503eceef78eb5e1151163afcc0e6a8de1f4
JamesLyddon <jameslyddon@gmail.com>
2022-11-12 22:31:43 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is your name?',
+    answer: 'Sir Galahad',
+    options: ['Sir Galahad', 'Sir Lancealot', 'King Arthur'],
+  },
+  {
+    id: 2,
+    question: 'What is your quest?',
+    answer: 'I seek the Grail',
+    options: ['I seek the Grail', 'To buy 10 fags', 'A VR headset?', 'To buy a vest'],
+  },
+  {
+    id: 3,
+    question: 'What is your favourite colour?',
+    answer: 'yellow',
+    options: ['red', 'blue', 'yellow'],
+  },
+]
